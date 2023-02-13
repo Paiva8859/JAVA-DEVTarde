dafeaafc75f4b3ae309884e64a90c660a70ec773
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-02-13 16:41:51 -0300

    13/02/23

--- CalculadoraVelocidadeScanner/App.java
+++ CalculadoraVelocidadeScanner/App.java
@@ -3,6 +3,6 @@ package CalculadoraVelocidadeScanner;
 public class App {
     public static void main(String[] args) {
         CalculadoraScanner objeto1= new CalculadoraScanner();
-        objeto1.calcular();
+        objeto1.calcular(); 
     }
 }
