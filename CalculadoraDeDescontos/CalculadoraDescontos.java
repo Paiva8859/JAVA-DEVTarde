2dc27b8b5741c5026efdd465166488658ad03d5b
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-02-13 15:09:45 -0300

    13/02/23

--- CalculadoraDeDescontos/CalculadoraDescontos.java
+++ CalculadoraDeDescontos/CalculadoraDescontos.java
@@ -1,17 +1,32 @@
 import javax.swing.plaf.synth.SynthScrollBarUI;
 
 public class CalculadoraDescontos {
+    public void calcula20(){
 System.out.println("==================");
 double precoProduto;
 precoProduto = 200;
-system.out.println("O preco do produto é de"+precoProduto+"reais");
+System.out.println("O preco do produto é de"+precoProduto+"reais");
 double percentualDesconto = 10;
 percentualDesconto = 20;
-system.out.println("O desconto será de"+percentualDesconto+"%");
+System.out.println("O desconto será de"+percentualDesconto+"%");
 double desconto = precoProduto*percentualDesconto/100;
 double precoComDesconto = precoProduto-desconto;
-system.out.println("com desconto, o produto sai por R$"+precoComDesconto+".");
-system.out.println("fim!");
-system.out.println("==================")
-
+System.out.println("com desconto, o produto sai por R$"+precoComDesconto+".");
+System.out.println("fim!");
+System.out.println("==================");
+    }
+public void calcula30(){
+    System.out.println("==================");
+    double precoProduto;
+    precoProduto = 200;
+    System.out.println("O preco do produto é de"+precoProduto+"reais");
+    double percentualDesconto = 10;
+    percentualDesconto = 30;
+    System.out.println("O desconto será de"+percentualDesconto+"%");
+    double desconto = precoProduto*percentualDesconto/100;
+    double precoComDesconto = precoProduto-desconto;
+    System.out.println("com desconto, o produto sai por R$"+precoComDesconto+".");
+    System.out.println("fim!");
+    System.out.println("==================");
+    }
 }
--- CalculadoraDeDescontos/Main.java
+++ CalculadoraDeDescontos/Main.java
@@ -4,6 +4,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        CalculadoraDescontos cDescontos = new CalculadoraDescontos();
+        cDescontos.calcula20();
+        cDescontos.calcula30();
     }
 }
