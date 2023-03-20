ac97ce183d57821ad45fb00bbf87b7a6ef433dc7
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-03-20 13:12:10 -0300

    20/03/23

--- EstudoEmCasa/App.java
+++ EstudoEmCasa/App.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         
         System.out.println("Qual Objeto executar?");
