b9018c1cff58c8e436dbf3abc6982fa0b6322988
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-05-08 13:29:50 -0300

    08/05/23

--- LacoRepeticaoFor/LacoRepeticaoFor.java
+++ LacoRepeticaoFor/LacoRepeticaoFor.java
@@ -47,81 +47,78 @@ public class LacoRepeticaoFor {
         String consoantes[] = new String[10];
 
         for (int i = 0; i < consoantes.length; i++) {
-            System.out.println("Insira a letra n° " + (i+1));
+            System.out.println("Insira a letra n° " + (i + 1));
             consoantes[i] = sc.nextLine();
         }
 
         for (int i = 0; i < consoantes.length; i++) {
-           switch (consoantes[i]) {
-            case "a":
-                n++;
-                break;
-            
-            case "e":
-                n++;
-                break;
+            switch (consoantes[i]) {
+                case "a":
+                    n++;
+                    break;
+
+                case "e":
+                    n++;
+                    break;
 
                 case "i":
-                n++;
-                break;
-            
-            case "o":
-                n++;
-                break;
+                    n++;
+                    break;
 
-            case "u":
-                n++;
-                break;
-           }
+                case "o":
+                    n++;
+                    break;
+
+                case "u":
+                    n++;
+                    break;
+            }
         }
 
-        System.out.println("O n° de consoantes inseridas é: " + (10-n));
+        System.out.println("O n° de consoantes inseridas é: " + (10 - n));
 
         System.out.println("As consoantes inseridas foram:");
         for (int i = 0; i < consoantes.length; i++) {
             switch (consoantes[i]) {
                 case "a":
                     break;
-                
+
                 case "e":
                     n++;
-    
-                    case "i":
+
+                case "i":
                     break;
-                
+
                 case "o":
                     break;
-    
+
                 case "u":
                     break;
 
                 default:
                     System.out.println(consoantes[i]);
-               }
+            }
         }
     }
 
     public void exercicio5() {
         double n[] = new double[20];
-        double par[] = new double[20];
-        double impar[] = new double[20];
         int nPar = 0;
         int nImpar = 0;
 
-
         for (int i = 0; i < n.length; i++) {
-            System.out.println("Insira o " + (i+1) + "° numero");
+            System.out.println("Insira o " + (i + 1) + "° numero");
             n[i] = sc.nextDouble();
 
             if (n[i] % 2 == 0) {
-                par[nPar] = n[i];
                 nPar++;
-            }
-            else{
-                impar[nImpar] = n[i];
+            } else {
                 nImpar++;
             }
         }
+
+        double par[] = new double[nPar];
+        double impar[] = new double[nImpar];
 
         System.out.println("=====================================================================================");
         System.out.println("Os numeros inseridos são:");
@@ -129,31 +126,29 @@ public class LacoRepeticaoFor {
             System.out.println(n[i]);
         }
 
+        System.out.println("=====================================================================================");
+        if (nPar > 0) {
 
-        if (nPar > 0){
-            System.out.println("=====================================================================================");
             System.out.println("Os números pares inseridos são:");
             for (int i = 0; i < nPar; i++) {
                 System.out.println(par[i]);
             }
-        }
-        else {
+        } else {
             System.out.println("Não foram inseridos numeros pares");
         }
 
+        System.out.println("=====================================================================================");
         if (nImpar > 0) {
-            System.out.println("=====================================================================================");
             System.out.println("Os numeros impares inseridos são:");
             for (int i = 0; i < nImpar; i++) {
                 System.out.println(impar[i]);
             }
-        } 
-        else {
+        } else {
             System.out.println("não foram inseridos numeros impares");
         }
     }
 
     public void exercicio6() {
-        
+
     }
 }
