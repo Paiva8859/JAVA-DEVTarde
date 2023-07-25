99837a5e97a5c3863a57a6a895d7b1be1df4dcdc
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2023-07-25 15:17:06 -0300

    25/07/23

---
+++ RevisaoFPOO/App.java
@@ -0,0 +1,13 @@
+package RevisaoFPOO;
+
+import RevisaoFPOO.Cadastro.Animais;
+
+/**
+ * App
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Animais obj1 = new Animais("pequeno", 7.55, "Paiva", "Macho", "cachorro");
+    }
+}
---
+++ RevisaoFPOO/Cadastro/Animais.java
@@ -0,0 +1,65 @@
+package RevisaoFPOO.Cadastro;
+
+public class Animais {
+    //ATRIBUTOS
+    String porte;
+    double peso;
+    String proprietario;
+    String genero;
+    String especie;
+    
+    //METODOS
+    public Animais(String porte, double peso, String proprietario, String genero, String especie) {
+        this.porte = porte;
+        this.peso = peso;
+        this.proprietario = proprietario;
+        this.genero = genero;
+        this.especie = especie;
+    }
+
+    public Animais(){
+        
+    }
+
+    public String getPorte() {
+        return porte;
+    }
+
+    public void setPorte(String porte) {
+        this.porte = porte;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(String proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+    
+    
+}
